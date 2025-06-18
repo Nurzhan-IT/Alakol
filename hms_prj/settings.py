0603da095cb21d26e1b400aef69d21cef9d4e016
Nurzhan <12farit21@gmail.com>
2025-06-19 00:31:34 +0500

    dashboard adjust; review russian translate; search pagination

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -122,7 +122,7 @@ WSGI_APPLICATION = 'hms_prj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'alakol',
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
