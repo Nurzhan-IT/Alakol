e4474e2f4f0878436bb6afa1350f418c59814406
Nurzhan <12farit21@gmail.com>
2025-07-11 16:02:18 +0500

    read debug.log in utf-8; selected_rooms.html ---> rewrite async version

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -447,6 +447,7 @@ LOGGING = {
             'class': 'logging.FileHandler',
             'filename': 'debug.log',
             'formatter': 'verbose',
+            'encoding': 'utf-8',
         },
         'console': {
             'level': 'DEBUG',
