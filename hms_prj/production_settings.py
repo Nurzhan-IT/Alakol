00f50e1f293f6e02fb9e02361354c6588e6965ef
Nurzhan <12farit21@gmail.com>
2025-08-13 07:10:09 +0500

    поменял порядок языков на кз, ру , ен

--- hms_prj/production_settings.py
+++ hms_prj/production_settings.py
@@ -220,8 +220,8 @@ LANGUAGE_COOKIE_SAMESITE = 'Lax'
 
 # Настройки для django-modeltranslation
 MODELTRANSLATION_DEFAULT_LANGUAGE = 'ru'  # По умолчанию русский
-MODELTRANSLATION_FALLBACK_LANGUAGES = ('ru', 'kk', 'en')  # Порядок резервных языков
-MODELTRANSLATION_LANGUAGES = ('ru', 'kk', 'en')  # Поддерживаемые языки для переводов
+MODELTRANSLATION_FALLBACK_LANGUAGES = ('kk', 'ru', 'en')  # Порядок резервных языков
+MODELTRANSLATION_LANGUAGES = ('kk', 'ru', 'en')  # Поддерживаемые языки для переводов
 
 # Настройки валидации переводов
 MODELTRANSLATION_ENABLE_FALLBACKS = True  # Разрешить использование резервных языков
